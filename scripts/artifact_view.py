42a98fcf5edc368445707706f86a002fca95f272
attayeb <attayebmohsen@gmail.com>
2021-10-14 23:26:02 +0900

    commit

--- scripts/artifact_view.py
+++ scripts/artifact_view.py
@@ -21,8 +21,21 @@ def export(artifact, filename, filetype):
         df.to_csv(filename)
 
     if filetype=="biom":
+        taxonomy_levels = ['kingdum', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+        art = Artifact.load(artifact).view(biom.Table)
+        meta_ = {x['id']: x['id'].split(";") for x in json.loads(art.to_json("QIIME2"))['rows']}
+
+        meta__ = {}
+        
+        for k, v in meta_.items():
+            current = {}
+            for i in range(len(v)):
+                current[taxonomy_levels[i]] = v[i]
+                meta__.update({k:current})
+        
+        art.add_metadata(meta__, axis='observation')
         with open(filename, "w") as f:
-            f.write(Artifact.load(artifact).view(biom.Table).to_json(generated_by="QIIME2"))
+            f.write(f, art.to_json(generated_by="QIIME2"))
 
 
 if __name__ == "__main__":
