3a993eb86589a705951e10ae815d34f69aed99c1
attayeb <attayebmohsen@gmail.com>
2021-10-28 10:42:37 +0900

    add manta script

--- scripts/manta.py
+++ scripts/manta.py
@@ -44,4 +44,5 @@ def manta(input_file, output_file, taxonpath, names, database):
     df3m = df3m[df3m['value']!= 0]
     df3m.to_csv(output_file, header=None, index=None, sep="\t")
 
-
+if __name__ == "__main__":
+    manta()
