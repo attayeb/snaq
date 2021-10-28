def1e13c74fe6cbcb7f2a77c7dd27289ffd1e4f3
attayeb <attayebmohsen@gmail.com>
2021-10-28 17:34:47 +0900

    add beta_diversity

--- scripts/beta_diversity.py
+++ scripts/beta_diversity.py
@@ -1,23 +1,21 @@
 import click
 from qiime2 import Artifact
 from qiime2 import Metadata
+from skbio import DistanceMatrix
 import pandas as pd
-from qiime2.plugins.diversity.pipelines import alpha
+from qiime2.plugins.diversity.pipelines import beta
 
 @click.command()
 @click.option("--inp")
-@click.option("--metrics", default='simpson,chao1,shannon,observed_features')
+@click.option("--metrics", default=['braycurtis,jaccard'])
 @click.option("--outp")
 def alpha_diversity(inp, metrics, outp):
     a = Artifact.load(inp)
     _metrics=metrics.split(",")
-    ret = []
 
     for metric in _metrics:
-        r = alpha(a, metric)
-        ret.append(r.alpha_diversity.view(Metadata).to_dataframe())
-
-    x = pd.concat(ret, axis=1)
-    Metadata(x).save(outp)
+        r = beta(a, metric)
+        r.view(DistanceMatrix).to_data_frame().to_csv(outp.split(".")[0]+"-" + 
+            metric + "."+outp.split(".")[1])
 if __name__ == "__main__":
     alpha_diversity()
