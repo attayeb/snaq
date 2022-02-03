ee28aca6a708bcc6f58910e37acdaf5b0adfa706
attayeb <attayebmohsen@gmail.com>
2022-02-03 16:19:17 +0900

    plot dada stats

--- scripts/plot_dada.py
+++ scripts/plot_dada.py
@@ -14,7 +14,8 @@ def plot_dada(inp, plot):
     df = art.view(Metadata).to_dataframe()
     plt.figure(figsize=(5, 5), dpi=100)
     sns.displot(x='percentage of input non-chimeric', data=df)
-    plt.title(plot.split("/")[-1].replace("+dada_stats.jpg", ""))
+    plt.title(plot.split("/")[-1].replace("+dd_stats.jpg", ""))
+    plt.xlim([0, 100])
     plt.tight_layout()
     plt.savefig(plot)
 
