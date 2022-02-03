fef3e0b60ed28bf69409a4774906e0448c5eda74
attayeb <attayebmohsen@gmail.com>
2022-02-03 10:37:51 +0900

    modify bb to bb-

--- scripts/export_phyloseq.R
+++ scripts/export_phyloseq.R
@@ -14,7 +14,9 @@ opt = parse_args(opt_parser)
 
 #print(opt$biom)
 ps <- phyloseq::import_biom(opt$biom, treefilename=opt$tree)
-df <- tidyr::separate(as.data.frame(tax_table(ps)), "Rank1", c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7"), sep=";")
-tax_table(ps) <- apply(df, 2, trimws)
+df <- tidyr::separate(as.data.frame(tax_table(ps)), "Rank1", c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Rank8", "Rank9", "Rank10"), sep=";")
+not_all_na <- function(x) any(!is.na(x))
+
+tax_table(ps) <- apply(df%>%select(where(not_all_na)), 2, trimws)
 
 saveRDS(ps, opt$outp)
