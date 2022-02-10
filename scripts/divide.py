0acad7ee30be208f25316da36818d2b1160c778c
attayeb <attayebmohsen@gmail.com>
2022-02-10 15:36:34 +0900

    Add logo

--- scripts/divide.py
+++
@@ -1,9 +0,0 @@
-import click
-import os
-
-@click.command()
-@click.option("-i", "origin", required=True, type=str)
-@click.option("-o", "target", required=True, type=str)
-@click.option("-n", "number", required=True, type=int)
-def divide(origin, target, number):
-    file_names = 
